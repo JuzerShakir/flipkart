6f790411a3a58643678b05167b24578d72e6c609
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-09-19 15:46:56 +0530

    Add & Setup `annotate`

--- app/models/account_block/account.rb
+++ app/models/account_block/account.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: accounts
+#
+#  id              :integer          not null, primary key
+#  email           :string           default(""), not null
+#  first_name      :string
+#  last_name       :string
+#  password_digest :string           default(""), not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_accounts_on_email  (email) UNIQUE
+#
 class AccountBlock::Account < ApplicationRecord
   has_secure_password
 end
