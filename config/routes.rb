fbb947f6e46af8bf2c4bf55d166b59649e3834b1
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-09-19 15:38:02 +0530

    Add routes for `accounts`

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :account_block do
+    resources :accounts, except: %i[new edit]
+  end
 end
