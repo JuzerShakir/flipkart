7c379164da8692c60b002c6690494a85d9854206
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-09-19 16:12:33 +0530

    Create `create` action for `AccountsController`

--- app/controllers/account_block/accounts_controller.rb
+++ app/controllers/account_block/accounts_controller.rb
@@ -1,2 +1,17 @@
 class AccountBlock::AccountsController < ApplicationController
+  def create
+    @account = AccountBlock::Account.new(account_params)
+
+    if @account.save
+      render json: @account, status: :created
+    else
+      render json: { errors: @account.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def account_params
+    params.permit(:email, :password)
+  end
 end
