6d6af8868c6ad3f8ead417e8e179ac5c10ffc941
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-09-20 11:30:46 +0530

    Setup `product` for `activeadmin`

--- app/admin/product_block_products.rb
+++ app/admin/product_block_products.rb
@@ -1,18 +1,33 @@
 ActiveAdmin.register ProductBlock::Product do
+    # skip authentication for admin
+    controller { skip_before_action :authenticate_account! }
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :title, :description, :price
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:title, :description, :price]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  # top nav
+  menu priority: 4
+
+  # whitelist attributes
+  permit_params :title, :description, :price
+
+
+  # index table
+  config.sort_order = 'title_asc'
+  index do
+    selectable_column
+    column :title do
+      truncate(_1.title, omission: "...", length: 30)
+    end
+    column :description do
+      truncate(_1.description, omission: "...", length: 50)
+    end
+    column :price
+    actions
+  end
+
+  # filter
+  filter :title_cont, label: "Title"
+  filter :description_cont, label: "Description"
+  filter :price
+
+  # form
+  config.create_another = true
 end
--- app/models/product_block/product.rb
+++ app/models/product_block/product.rb
@@ -10,4 +10,8 @@
 #  updated_at  :datetime         not null
 #
 class ProductBlock::Product < ApplicationRecord
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["title", "description", "price"]
+  end
 end
