1db87529e554a232049084177e8c74c227fbb288
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-09-20 13:36:02 +0530

    Add validations tests for `accounts` attributes

---
+++ spec/models/account_block/account_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe AccountBlock::Account, type: :model do
+  describe "validations" do
+    context "for email" do
+      # email address
+      it { should validate_presence_of(:email) }
+      it { should validate_uniqueness_of(:email) }
+      it { should validate_email_format_of(:email) }
+    end
+
+    # password
+    context "for password" do
+      it { should validate_presence_of(:password) }
+      it { should validate_length_of(:password).is_at_least(6) }
+    end
+  end
+end
