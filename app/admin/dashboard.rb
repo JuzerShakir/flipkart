049c72a33ade31c52fe0f70764da646df677494c
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-09-20 11:45:44 +0530

    Show `accounts` & `products` in dashboard page

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -6,12 +6,20 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
-    panel "Recent Products" do
-      # ul do
-      #   Product.order(created_at: :desc).limit(5).map do |product|
-      #     li link_to(product.title, admin_product_path(product))
-      #   end
-      # end
+    panel "Recent Accounts" do
+      ul do
+        AccountBlock::Account.order(created_at: :desc).limit(5).map do |account|
+          li link_to(account.email, admin_account_block_account_path(account))
+        end
+      end
     end
-  end # content
+
+    panel "Recent Products" do
+      ul do
+        ProductBlock::Product.order(created_at: :desc).limit(5).map do |product|
+          li link_to(product.title, admin_product_block_product_path(product))
+        end
+      end
+    end
+  end
 end
